65c815d0e486d862b66a99796e6b3cc477b6082f
Vivek Pathak <82827056+vikpthk02@users.noreply.github.com>
2022-10-25 17:17:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const Access_Key = 'R1V2oOYTU3GoladfRiEeIAd5qOVVJXI1xSKCbkKWCcQ';
+  const [value, setValue] = useState("");
+  const [results, setResults] = useState([]);
+  const [randomResults, setRandomResults] = useState([]);
+
+  const fetchData = () => {
+    fetch(`https://api.unsplash.com/search/photos?client_id=${Access_Key}&per_page=50&query=${value}&orientation=squarish&show_on_profile`)
+      .then(res => res.json())
+      .then(data => {
+        setResults(data.results)
+      });
+  }
+  const fetchRendomData = () => {
+    fetch(`https://api.unsplash.com/search/photos?client_id=${Access_Key}&per_page=500&query=trending&orientation=squarish`)
+      .then(res => res.json())
+      .then(data => {
+        setRandomResults(data.results)
+      });
+  }
+
+  useEffect(() => {
+    fetchRendomData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header-section">
+        <div className="header-content">
+          <h5 className="logo">Unsplash Clone</h5>
+          <p className="slogan">The internet’s source for visuals. Powered by creators everywhere.</p>
+
+          <form>
+            <input type="text"
+              name="Search"
+              className="search-box"
+              placeholder="Search free hight-resolution photos"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              onSubmit={fetchData()}
+            />
+            {/* <button className="search-btn"
+              onClick={fetchData()} type="submit"
+            >Search</button> */}
+          </form>
+        </div>
       </header>
-    </div>
+
+      <div className="lists">
+        {
+          results.map((users) => {
+            return <img className="item" key={users.id} src={users.urls.regular} />
+          })
+        }
+      </div>
+      <div className="lists">
+        {
+          randomResults.map((users) => {
+            return <img className="item" key={users.id} src={users.urls.regular} subtitle={<span>by: Author</span>} />
+          })
+        }
+      </div>
+
+    </>
   );
 }
 
 export default App;
+
+{/* <div>
+        <input type="text" onChange={(e) => setValue(e.target.value)} value={value} />
+        <button onClick={fetchData()} type="submit">Search</button>
+      </div> */}
